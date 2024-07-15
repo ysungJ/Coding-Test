ca783f1dc70746dba2ec5e2602252cc70c1fea6c
Yu-Sung <120106925+ysungJ@users.noreply.github.com>
2024-07-15 23:23:07 +0900

    백준1330 두 수 비교하기
    문제 : 
    두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.

---
+++ BaekJoon/B1330.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int A, B;
+	cin >> A >> B;
+
+	if (A > B) {
+		cout << ">" << endl;
+	
+	}
+	if (A < B) {
+		cout << "<" << endl;
+	}
+	if (A == B) {
+		cout << "==" << endl;
+	}
+
+	return 0;
+}
