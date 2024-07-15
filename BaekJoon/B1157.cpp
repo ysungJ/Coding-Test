903e58b38728530d0ada74e7d52d0b6731c7d442
Yu-Sung <120106925+ysungJ@users.noreply.github.com>
2024-07-15 23:19:39 +0900

    백준 1157 단어공부
    알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다

---
+++ BaekJoon/B1157.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    int max = 0;
+    int idx = 0;
+    int arr[26] = {0,};
+    string S;
+    const string a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+    cin >> S;
+    for(int i = 0 ; i < S.length(); i++){
+        S[i] = toupper(S[i]);
+        arr[a.find(S[i])]++;
+    }
+    for(int j = 0; j< sizeof(arr)/sizeof(int) ; j ++){
+        if(arr[j] == 0)
+            continue;
+        if(max == arr[j]){
+            idx = 26;
+            continue;
+        }
+        if(max < arr[j]){
+            max = arr[j];
+            idx = j;
+        }
+    }
+    cout << a[idx];
+}
