4776007c1897808577ca3a2e5ec3626f7e5d4265
Yu-Sung <120106925+ysungJ@users.noreply.github.com>
2024-07-26 17:52:49 +0900

    2753 윤년
    연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
    
    윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
    
    예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 1900년은 100의 배수이고 400의 배수는 아니기 때문에 윤년이 아니다. 하지만, 2000년은 400의 배수이기 때문에 윤년이다.

---
+++ BaekJoon/B2753.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int year;
+	cin >> year;
+	
+	if (year % 4 == 0 && year % 100 != 0)
+		cout << 1;
+	else if (year % 400 == 0)
+		cout << 1;
+	else cout << 0;
+
+	return 0;
+}
