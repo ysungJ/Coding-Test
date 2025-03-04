ed520126fed7af40e74e7b12de4105b0ca6d862c
Vidan <120106925+ysungJ@users.noreply.github.com>
2025-03-04 13:53:03 +0900

    11382번 꼬마정민

---
+++ BaekJoon/B11382.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+   long long A, B, C;
+   A = 0;
+   B = 0;
+   C = 0;
+   
+   scanf ("%lld %lld %lld", &A, &B, &C);
+   
+   printf("%lld", A+B+C);
+
+    return 0;
+}
